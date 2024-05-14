f7c776b8e5f2ed780ad220ee8756c2175552c402
adigarg712 <123372088+adigarg712@users.noreply.github.com>
2024-05-14 09:14:20 +0530

    Add files via upload

---
+++ 8085main.cpp
@@ -0,0 +1,84 @@
+#include "Debugger.h"
+int main(int argc, char** argv)
+{
+    int pc;
+    char ch,f,c;
+    dp = 0;
+    set_instructions_size();
+    string line="";
+    cout<<"******\tWelcome To 8085 Microprocessor Emulator******\t\n\n";
+    cout<<"Press:\n2 to write code\n3 to Exit\n\n";
+    do
+    {
+        cout<<"Enter:\n";
+        cin>>ch;
+        fflush(stdin);
+        if(!(ch=='1'||ch=='2' || ch=='3'))
+        {
+            cout<<"Invalid Input. Try Again !\n";
+        }
+    }while(!(ch=='1' || ch=='2' || ch=='3'));
+    if(ch=='3')
+    {
+        cout<<"\nThank You for using 8085 Emulator!!\n";
+        return 0;
+    }
+    if(ch=='2')//INPUT FROM CONSOLE
+    {
+        cout<<"Enter starting address:\n";
+        remove("previousConsoleCode.txt");
+        ofstream code;
+        code.open("previousConsoleCode.txt",ios::out);
+        fflush(stdin);
+        getline(cin,start_address);
+        code<<start_address<<endl;
+        program_counter = start_address;
+        do
+        {
+            ++number_of_lines;
+            cout<<program_counter<<": ";
+            getline(cin,line);
+            if(!check0(line))
+            {
+                if(inst_size[line.substr(0,indexOf(line,' '))] == 0)
+                {
+                    --number_of_lines;
+                }
+            }
+            RAM[program_counter] = line;
+            code<<line<<endl;
+            pc = hextodec(program_counter);
+            if(line.size()==3 || line.size()==4 || line.size()==2)
+            {
+                pc += inst_size[line];
+            }
+            else
+            {
+                pc += inst_size[line.substr(0,indexOf(line,' '))];
+            }
+            program_counter = dectohex(pc);
+        }while(line!="HLT" && line!="EOF");
+        ++number_of_lines;
+        code.close();
+    }
+    do
+    {
+        cout<<"\nDebugger:\n1) ON\n2) OFF\n\nEnter:\n";
+        cin>>f;
+        if(f!='1' && f!='2')
+        {
+            cout<<"\nInvalid Option! Try Again!\n";
+        }
+    }while(f!='1' && f!='2');
+    if(f=='2')
+    {
+        set_instructions();
+        reset_flags();
+        reset_registers();
+        program_counter = start_address;
+        stepORrun(2);
+        return 0;
+    }
+    debugger(int(ch-'0'));
+    return 0;
+}
---
+++ Debugger.h
@@ -0,0 +1,8 @@
+#include "data_transfer_header.h"
+#include "Debugger.cpp"
+void display();
+void break_point_set(int);
+void help();
+void print();
+void stepORrun(int);
+void debugger(int);
